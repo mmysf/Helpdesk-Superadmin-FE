55967518db8a865c58ebae78e7057bac651762c6
yudhaperwira17 <116625101+yudhaperwira17@users.noreply.github.com>
2025-03-04 19:17:06 +0700

    fix (order) : ui adjusment

---
+++ _app/services/remote/repository/order/index.service.ts
@@ -0,0 +1,45 @@
+import { useHttp, useHttpMutation } from "@/root/_app/hooks/http";
+import { ORDER } from "..";
+import { ServiceConfig, ServiceMutationConfig } from "../types";
+import {
+  HistoryHourListResponse,
+  OrderDetailResponse,
+  OrderPaymentUpdateStatusPayload,
+  OrderPaymentUpdateStatusResponse,
+} from "./type";
+
+export const useOrderList = (
+  config?: ServiceConfig<HistoryHourListResponse>,
+) => {
+  return useHttp(ORDER.LIST, {
+    httpOptions: config?.axios,
+    queryOptions: config?.query,
+  });
+};
+
+export const useOrderDetail = (
+  id: string,
+  config?: ServiceConfig<OrderDetailResponse>,
+) => {
+  return useHttp(ORDER.DETAIL(id), {
+    httpOptions: config?.axios,
+    queryOptions: config?.query,
+  });
+};
+
+export const useOrderStatusPayment = (
+  id: string,
+  config?: ServiceMutationConfig<
+    OrderPaymentUpdateStatusResponse,
+    OrderPaymentUpdateStatusPayload
+  >,
+) => {
+  return useHttpMutation<
+    OrderPaymentUpdateStatusResponse,
+    OrderPaymentUpdateStatusPayload
+  >(ORDER.UPDATE_PAYMENT(id), {
+    method: "PATCH",
+    httpOptions: config?.axios,
+    queryOptions: config?.query,
+  });
+};
